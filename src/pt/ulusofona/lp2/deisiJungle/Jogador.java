7f5806c5943949f10925ae5dfb17d1eee3f93c4e
Lodney Santos <a21505293@alunos.ulht.pt>
2023-07-11 17:21:54 +0100

    > impl. metodo caracterizarEspecieJogador

--- src/pt/ulusofona/lp2/deisiJungle/Jogador.java
+++ src/pt/ulusofona/lp2/deisiJungle/Jogador.java
@@ -26,36 +26,54 @@ public class Jogador {
                         180, 4, 10, 1, 6);
                 jogadorAtual.setEspecie(this.especie);
                 jogadorAtual.setEnergiaAtual(this.especie.getEnergiaInicial());
+                jogadorAtual.getEspecie().setConsumoEnergia(4);
+                jogadorAtual.getEspecie().setVelocidadeMinima(1);
+                jogadorAtual.getEspecie().setVelocidadeMaxima(6);
             }
             case "L" -> {
                 this.especie = new Leao(idEspecie, "Leão", "lion.png",
                         80, 2, 10, 4, 6);
                 jogadorAtual.setEspecie(this.especie);
                 jogadorAtual.setEnergiaAtual(this.especie.getEnergiaInicial());
+                jogadorAtual.getEspecie().setConsumoEnergia(2);
+                jogadorAtual.getEspecie().setVelocidadeMinima(4);
+                jogadorAtual.getEspecie().setVelocidadeMaxima(6);
             }
             case "P" -> {
                 this.especie = new Passaro(idEspecie, "Pássaro", "bird.png",
                         70, 4, 50, 5, 6);
                 jogadorAtual.setEspecie(this.especie);
                 jogadorAtual.setEnergiaAtual(this.especie.getEnergiaInicial());
+                jogadorAtual.getEspecie().setConsumoEnergia(4);
+                jogadorAtual.getEspecie().setVelocidadeMinima(5);
+                jogadorAtual.getEspecie().setVelocidadeMaxima(6);
             }
             case "T" -> {
                 this.especie = new Tartaruga(idEspecie, "Tartaruga", "turtle.png",
                         150, 1, 5, 1, 3);
                 jogadorAtual.setEspecie(this.especie);
                 jogadorAtual.setEnergiaAtual(this.especie.getEnergiaInicial());
+                jogadorAtual.getEspecie().setConsumoEnergia(1);
+                jogadorAtual.getEspecie().setVelocidadeMinima(1);
+                jogadorAtual.getEspecie().setVelocidadeMaxima(3);
             }
             case "Z" -> {
                 this.especie = new Tarzan(idEspecie, "Tarzan", "tarzan.png",
                         70, 2, 20, 1, 6);
                 jogadorAtual.setEspecie(this.especie);
                 jogadorAtual.setEnergiaAtual(this.especie.getEnergiaInicial());
+                jogadorAtual.getEspecie().setConsumoEnergia(2);
+                jogadorAtual.getEspecie().setVelocidadeMinima(1);
+                jogadorAtual.getEspecie().setVelocidadeMaxima(6);
             }
             case "U" -> {
                 this.especie = new Unicornio(idEspecie, "Unicórnio", "unicorn.png",
-                        200, 8, 20, 3, 3);
+                        200, 8, 20, 3, 6);
                 jogadorAtual.setEspecie(this.especie);
                 jogadorAtual.setEnergiaAtual(this.especie.getEnergiaInicial());
+                jogadorAtual.getEspecie().setConsumoEnergia(8);
+                jogadorAtual.getEspecie().setVelocidadeMinima(3);
+                jogadorAtual.getEspecie().setVelocidadeMaxima(6);
             }
         }
 
